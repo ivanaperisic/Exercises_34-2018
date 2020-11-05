5849f38bd5f203fd92416306d213531cec2c941e
DESKTOP-EBD3442\HP <34-2018@ftn.edu.rs>
2020-11-05 19:42:17 +0100

    Model klasa  kreirana

---
+++ Exercises_34-2018/Exercises_34-2018/ExerciseResults.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercises_34_2018
+{
+    class ExerciseResults { 
+      public ExerciseResults(int id, string studentName, string studentIndex, int points)
+    {
+        Id = id;
+        StudentName = studentName;
+        StudentIndex = studentIndex;
+        Points = points;
+    }
+    {
+    
+
+        public int Id { get; set; }
+        public string StudentName { get; set; }
+
+        public string StudentIndex { get; set; }
+
+        public int Points { get; set; }
+    }
+
+
+}
